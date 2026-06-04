55ff59ef225b8fa1520378297093aa3c7136251b
romer8 <gio.rombus@gmail.com>
2026-06-04 14:21:36 -0600

    docker compose takes care of watching db for healthy

--- scripts/run.sh
+++ scripts/run.sh
@@ -58,44 +58,6 @@ done
 
 echo_status "Starting up..."
 
-
-if [[ $test = false ]]; then
-  # Set extra ENVs
-  export NGINX_USER=$(grep 'user .*;' /etc/nginx/nginx.conf | awk '{print $2}' | awk -F';' '{print $1}')
-
-  # Apply States
-  if [[ $skip_db_setup != true ]]; then
-    echo_status "Checking if DB is ready"
-    if [[ $db_engine == "django.db.backends.postgresql" ]]; then
-        # Create Salt Config for PostgreSQL
-        echo "postgres.host: '${TETHYS_DB_HOST}'" >> /etc/salt/minion
-        echo "postgres.port: '${TETHYS_DB_PORT}'" >> /etc/salt/minion
-        echo "postgres.user: '${TETHYS_DB_USERNAME}'" >> /etc/salt/minion
-        echo "postgres.pass: '${TETHYS_DB_PASSWORD}'" >> /etc/salt/minion
-        echo "postgres.bins_dir: '${CONDA_HOME}/envs/${CONDA_ENV_NAME}/bin'" >> /etc/salt/minion
-
-        db_check_count=0
-
-        until ${CONDA_HOME}/envs/${CONDA_ENV_NAME}/bin/pg_isready -h ${TETHYS_DB_HOST} -p ${TETHYS_DB_PORT} -U postgres; do
-          if [[ $db_check_count -gt $db_max_count ]]; then
-            >&2 echo "DB was not available in time - exiting"
-            exit 1
-          fi
-          >&2 echo "DB is unavailable - sleeping"
-          db_check_count=`expr $db_check_count + 1`
-          sleep 5
-        done
-      
-
-    else
-      echo_status "Using SQLite3 as the database"
-    fi
-  else
-    # Database setup should be skipped
-    echo "Skipping database setup: check SKIP_DB_SETUP environment variable."
-  fi
-fi
-
 echo_status "Setting up Tethys... (This might take a bit)"
 
 ./init_tethys.sh
