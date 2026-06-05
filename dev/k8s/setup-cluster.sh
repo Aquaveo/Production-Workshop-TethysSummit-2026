c445b285c1782eb1bfb3f7e441f15e009e4b0bf6
romer8 <gio.rombus@gmail.com>
2026-06-05 12:50:13 -0600

    Fix setup script to ensure tethys-storage directory is created before cluster creation

--- dev/k8s/setup-cluster.sh
+++ dev/k8s/setup-cluster.sh
@@ -5,6 +5,7 @@ echo "Installing k3d . . ."
 wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | bash
 echo "k3d installed, creating cluster . . ."
 
+mkdir -p "$HOME/k3d/tethys-storage"
 k3d cluster create tethys \
   --servers 1 \
   --agents 1 \
